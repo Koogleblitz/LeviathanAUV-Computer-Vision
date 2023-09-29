9211e1d335aeaa7a519452072cf3d9770e0a8e1e
koogleblitz <richardtobing@outlook.com>
2023-09-28 22:44:30 -0700

    created a file that uses the usb camera on the jetson to (supposedly) record video. The resulting recording could not be read by mpv, mplayer, or opencv (these video players have been tested on another video). As a hotfix, the file saves each individual frame as a seperate png, this is horribly inefficient because it sums up to 1 mb per png. this will make data aquisition harder, should fix later

--- camRecTest.py
+++ camRecTest.py
@@ -1,30 +1,47 @@
-#ou can use OpenCV to record video from your USB camera. Here is a simple Python script that uses OpenCV to capture video from a USB camera and save it to a file
+# [+:] You can use OpenCV to record video from your USB camera. 
+    # Here is a simple Python script that uses OpenCV to capture video from a USB camera and (supposed to) save it to a file
+    # But having issues recording, so i'm going to save each frame individually as a pic
 
 import cv2
+import os
+
+if not os.path.exists('frames'): 
+    os.makedirs('frames')
+
 
 # Create a VideoCapture object
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
-# Define the codec and create a VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+# Define the codec and create a VideoWriter object if recording
+#recorder = cv2.VideoWriter_fourcc(*'mp4v')
+#out = cv2.VideoWriter('output3.mp4', recorder, 20.0, (640, 480))
 
-while(cap.isOpened()):
+frameLim= 1000
+
+
+#while(cap.isOpened()):
+i=0
+while(i<=frameLim):
     ret, frame = cap.read()
     if ret == True:
-        # Write the frame into the file 'output.avi'
-        out.write(frame)
+        # Write the frame into the file 'output.avi' (not working curently)
+        # out.write(frame)
+
+        #[+] Hotfix: save each frame individually
+        cv2.imwrite(f'frames/frame_{i}.png', frame)
+        i+=1 
 
         # Display the resulting frame
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame_{i}', frame)
 
-        # Quit if 'q' is pressed
+ 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
+        print("[x:] ERROR: No frames read. ")
         break
 
 # Release everything when done
 cap.release()
-out.release()
+#out.release()
 cv2.destroyAllWindows()
