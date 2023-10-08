d9fc86060fc8e7ad7f8d3d251c2ca3eb0ed9cc09
koogleblitz <richardtobing@outlook.com>
2023-10-07 22:29:08 -0700

    Works on tx1, still getting the repeating frame glitch. Going to switch to v4ls recording for usb camera using gstreamer

--- camRecTest.py
+++ camRecTest.py
@@ -16,11 +16,6 @@ if not os.path.exists('recordings'):
 
 
 
-#cap = cv2.VideoCapture(2)
-# Define the codec and create a VideoWriter object if recording
-#recorder = cv2.VideoWriter_fourcc(*'mp4v')
-#out = cv2.VideoWriter('output3.mp4', recorder, 20.0, (640, 480))
-
 #recording_path= r'C:\Users\richa\OneDrive\CSEC_Robosub\richardCode\Repo\recordings'
 #vid_Path= r"C:\Users\richa\OneDrive - email.ucr.edu\CSEC_Robosub\richardCode\Repo\recordings\\"
 #frame_Path=  r"C:\Users\richa\OneDrive - email.ucr.edu\CSEC_Robosub\richardCode\Repo\frames"
@@ -34,37 +29,44 @@ dateTime= datetime.now().strftime("%Y%m%d_%H%M%S")
 #[+] at frameDiv=2 and frmLim=200, it records at 16-20 fps for ~10 seconds. 
     # Oddly, the png frames yield a smaller file size than the jpg frames. I see no reason to use the jpg frames. 
     # A 10s vid would be ~500kb for jpg and ~400kb for png. 
-    # On the TX1, using the default black carrier board, with the cam plugged into the OTG socket, the camNum==2
-frmLim= 200
+    # On the TX1, using the default black carrier board, with the cam plugged into the OTG socket, the camNum==2OTG, still need to test using the orbitty and using the usb hub
+frmLim= 50
 frmDiv= 2
 frmSize= (480,640)
 fps= 18
 imgType= ".png"
-camNum= 2
+camNum= 1
 
 
-
+cap = cv2.VideoCapture(camNum)
 def record_vid():
     print('    --> recording fps: ', fps)
     print('    --> recording imgType: ', imgType)
     print('\n')
+    
+    
+    # Define the codec and create a VideoWriter object if recording
+    #recorder = cv2.VideoWriter_fourcc(*'mp4v')
+    #out = cv2.VideoWriter('output.mp4', recorder, 20.0, (480, 640))
 
     i=0
     #while(cap.isOpened()):
-    cap = cv2.VideoCapture(camNum)
     while(i<=frmLim*frmDiv):
         ret, frame = cap.read()
         if ret == True:
             # Write the frame into the file 'output.avi' (not working curently)
             # out.write(frame)
+
             #[+] Hotfix: save each frame individually
             if i%frmDiv==0: cv2.imwrite(f'frames/frame_{i//frmDiv}'+imgType, frame)
-            i+=1 
+            i+=1
+             
 
             # Display the resulting frame
             cv2.putText(frame, '[::'+str(i)+']', org=(10, 20), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=.5, color= (100,100,255), thickness=2)
             cv2.imshow('frame_', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'): break
+            
         else:
             print("    \n--> [x:] ERROR: No camera frames read. \n")
             break
@@ -80,12 +82,13 @@ def compile_vid(image_folder, video_name, fps=18, imgType= ".png"):
     images = [img for img in os.listdir(image_folder) if img.endswith(imgType)]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
-    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
+    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), 18, (width,height))
 
     frameCnt= len(images)
     for i, image in enumerate(images):
+        if (i%20)==0 or i==frameCnt: print("Compiled: --> ", i, " / ", frameCnt)
         video.write(cv2.imread(os.path.join(image_folder, image)))
-        if (i%10)==0 or i==frameCnt: print("Compiled: --> ", i, " / ", frameCnt)
+        
 
     # cv2.destroyAllWindows()
     video.release()
@@ -104,12 +107,13 @@ def del_frames():
             print(f'Failed to delete {file_path}. Reason: {e}')
 
 
-# [+] record video
 record_vid()
-# [+] Compile Video
 compile_vid(image_folder= frame_Path, 
                 video_name= vid_Path+dateTime+imgType+".mp4", 
                 fps=fps, 
                 imgType= imgType)
 del_frames()
 cv2.destroyAllWindows()
+
+
+# nvgstcapture-1.0 --camsrc=1 --cap-dev-node=2
